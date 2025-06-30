5bbf3695871d72d616ed1258cf027cc9261131eb
Lawrence Bass <lawmbass1@gmail.com>
2025-06-29 23:53:35 -0400

    Updates domain name
    Updates the domain name in the configuration.
    
    This ensures the application points to the correct domain.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
   appDescription:
     "The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app.",
   // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
-  domainName: "drone.sleepysquid.com",
+  domainName: "drones.sleepysquid.com",
   crisp: {
     // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (mailgun.supportEmail) otherwise customer support won't work.
     id: "",
