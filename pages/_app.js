8e7b5bb98d6ab0208926336b3691c70e51ffe528
Lawrence Bass <lawmbass1@gmail.com>
2025-07-29 23:37:37 -0400

    Refactor viewport meta tag handling by moving it from _app.js to _document.js for better mobile responsiveness management

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
 import '@/styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { DarkModeProvider } from '@/components/layout/DarkModeContext';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes" />
-      </Head>
-      <SessionProvider session={session}>
-        <DarkModeProvider>
-          <Component {...pageProps} />
-        </DarkModeProvider>
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      <DarkModeProvider>
+        <Component {...pageProps} />
+      </DarkModeProvider>
+    </SessionProvider>
   );
 } 
